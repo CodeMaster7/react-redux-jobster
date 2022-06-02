488e37c22bfa5e3260c2dd744537f95361ec7f13
sam <sammybonfanti@gmail.com>
2022-06-01 22:32:41 -0700

    ADD: normalize css global styles

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
-import './assets/css/index.css'
+import 'modern-normalize/modern-normalize.css'
+import './index.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
