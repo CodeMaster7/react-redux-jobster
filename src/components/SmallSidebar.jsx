3fbc82c2da5614cacee8a4f454e9b5ccc179fef7
sam <sammybonfanti@gmail.com>
2022-06-02 22:51:56 -0700

    ADD: small sidebar toggle and links file

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,17 +1,28 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { FaTimes } from 'react-icons/fa'
+import { toggleSidebar } from '../features/user/userSlice'
 import Wrapper from '../assets/wrappers/SmallSidebar'
 import Logo from './Logo'
 
 function SmallSidebar() {
+    const { isSidebarOpen } = useSelector((store) => store.user)
+    const dispatch = useDispatch()
+
+    const toggle = () => {
+		dispatch(toggleSidebar())
+	}
+
   return (
 		<Wrapper>
-			<div className='sidebar-container show-sidebar'>
+			<div
+				className={
+					isSidebarOpen
+						? 'sidebar-container show-sidebar'
+						: 'sidebar-container'
+				}>
 				<div className='content'>
-					<button
-						className='close-btn'
-						onClick={() => console.log('toggle')}>
+					<button className='close-btn' onClick={toggle}>
 						<FaTimes />
 					</button>
 					<header>
